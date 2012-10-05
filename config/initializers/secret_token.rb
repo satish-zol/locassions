84a9ce9a8eef68c00ddcba4bad3f2dc8535f549d
satish <satish@satish-virtual-machine.(none)>
2012-10-06 00:01:44 +0530

    Initial comment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Locassions::Application.config.secret_token = 'f29b96cace76a7d375aac380d03400111f6d7cca688ed4cf98f6bf8dfff2169da27a617c76df8c7dab2906ca04b337b5ff3d771c1a65cb0eb1b2669c905711c5'
